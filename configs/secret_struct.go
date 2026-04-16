7612dd0c5294d3850003be4452ee2f031c7ae8e0
Somphong Phuangmalai <somphong.ph@gmail.com>
2026-04-16 21:46:31 +0700

    update config

--- configs/secret_struct.go
+++ configs/secret_struct.go
@@ -1,12 +1,40 @@
 package configs
 
 type Secrets struct {
-	Redis Redis `mapstructure:",squash"`
+	// Redis Redis `mapstructure:",squash"`
+	// Mongo Mongo `mapstructure:",squash"`
+	// Postgres Postgres `mapstructure:",squash"`
+	// RabbitMQ RabbitMQ `mapstructure:",squash"`
 }
 
-type Redis struct {
-	Host         string `mapstructure:"redis_host"`
-	Port         int    `mapstructure:"redis_port"`
-	Password     string `mapstructure:"redis_password"`
-	InstanceName string `mapstructure:"redis_instance_name"`
-}
+// type Redis struct {
+// 	Host         string `mapstructure:"redis_host"`
+// 	Port         int    `mapstructure:"redis_port"`
+// 	Password     string `mapstructure:"redis_password"`
+// 	InstanceName string `mapstructure:"redis_instance_name"`
+// }
+
+// type Mongo struct {
+// 	Connection string `mapstructure:"mongo_connection"`
+// 	Options    string `mapstructure:"mongo_options"`
+// 	DbName     string `mapstructure:"mongo_db_name"`
+// 	IsDebug    bool   `mapstructure:"mongo_is_debug"`
+// }
+
+// type Postgres struct {
+// 	Host     string `mapstructure:"postgres_host"`
+// 	Port     int    `mapstructure:"postgres_port"`
+// 	Database string `mapstructure:"postgres_database"`
+// 	User     string `mapstructure:"postgres_user"`
+// 	Password string `mapstructure:"postgres_password"`
+// 	SSLMode  string `mapstructure:"postgres_ssl_mode"`
+// 	IsDebug  bool   `mapstructure:"postgres_is_debug"`
+// }
+
+// type RabbitMQ struct {
+// 	Host     string `mapstructure:"rabbitmq_host"`
+// 	Port     int    `mapstructure:"rabbitmq_port"`
+// 	User     string `mapstructure:"rabbitmq_user"`
+// 	Password string `mapstructure:"rabbitmq_password"`
+// 	VHost    string `mapstructure:"rabbitmq_vhost"`
+// }
--- internal/server/server.go
+++ internal/server/server.go
@@ -10,6 +10,10 @@ import (
 
 type server struct {
 	port int
+	// redis    xcache.RedisService
+	// mongo    xdb.MongoService
+	// postgres xdb.PostgresService
+	// rabbit   xmq.RabbitMQService
 }
 
 func NewServer() *http.Server {
@@ -20,9 +24,63 @@ func NewServer() *http.Server {
 		conf.App.Name,
 		conf.Server.PrettyPrint,
 	)
+	// ctx := context.Background()
+
+	// Redis
+	// var redisConfig xcache.RedisConfig
+	// if err := copier.Copy(&redisConfig, sect.Redis); err != nil {
+	// 	log.Fatalf("Failed to copy Redis config: %s", err)
+	// }
+	// redisService, err := xcache.NewRedis(ctx, &redisConfig)
+	// if err != nil {
+	// 	log.Fatalf("Failed to connect to Redis: %s", err)
+	// }
+
+	// MongoDB
+	// var mongoConfig xdb.MongoConfig
+	// if err := copier.Copy(&mongoConfig, sect.Mongo); err != nil {
+	// 	log.Fatalf("Failed to copy mongo config: %s", err)
+	// }
+	// mongoService, err := xdb.NewMongoService(ctx, &mongoConfig)
+	// if err != nil {
+	// 	log.Fatalf("Failed to connect to MongoDB: %s", err)
+	// }
+
+	// PostgreSQL
+	// var postgresConfig xdb.PostgresConfig
+	// if err := copier.Copy(&postgresConfig, sect.Postgres); err != nil {
+	// 	log.Fatalf("Failed to copy PostgreSQL config: %s", err)
+	// }
+	// postgresService, err := xdb.NewPostgresService(ctx, &postgresConfig)
+	// if err != nil {
+	// 	log.Fatalf("Failed to connect to PostgreSQL: %s", err)
+	// }
+
+	// PostgreSQL Migrations
+	// if err := migrations.RunMigrations(postgresService); err != nil {
+	// 	log.Fatalf("Failed to run migrations: %s", err)
+	// }
+
+	// RabbitMQ
+	// var rabbitConfig xmq.RabbitMQConfig
+	// if err := copier.Copy(&rabbitConfig, sect.RabbitMQ); err != nil {
+	// 	log.Fatalf("Failed to copy RabbitMQ config: %s", err)
+	// }
+	// rabbitMQService, err := xmq.NewRabbitMQ(ctx, &rabbitConfig)
+	// if err != nil {
+	// 	log.Fatalf("Failed to connect to RabbitMQ: %s", err)
+	// }
+
+	// RabbitMQ Consumer
+	// consumer := NewConsumer(rabbitMQService, postgresService)
+	// go consumer.StartConsumers(ctx)
 
 	newServer := &server{
 		port: conf.App.Port,
+		// redis:    redisService,
+		// mongo: mongoService,
+		// postgres: postgresService,
+		// rabbit:   rabbitMQService,
 	}
 
 	// Declare Server config
